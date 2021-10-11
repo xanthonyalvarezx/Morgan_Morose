cecc17822a5951d924d343cc872160d2bb1927e0
Anthony Alvarez <anthonyalvarez@Anthonys-iMac.home>
2021-10-11 09:54:58 -0400

    got the detail page to show post photos

--- landing/views.py
+++ landing/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from user_admin.models import Author, BlogPost
+import re
 # Create your views here.
 def landing(request):
     posts = BlogPost.objects.all()
@@ -7,6 +8,7 @@ def landing(request):
 
 def post_detail(request, post_id: int):
     posts = BlogPost.objects.get(id=post_id)
-    
+    searchImage = re.search("(images\/(\S+))",str(posts.image))
+    print(searchImage[2])
 
-    return render(request, "post_detail.html", {"posts": posts})
+    return render(request, "post_detail.html", {"posts": posts, "searchImage": searchImage[2]})
